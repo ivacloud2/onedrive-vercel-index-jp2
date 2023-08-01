bcfb17141dda7ca587811b6852b4a55ef1e1a05a
ivacloud2 <141173004+ivacloud2@users.noreply.github.com>
2023-08-01 17:52:34 -0500

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "Repositorio: ivanime",
+  title: "Repositorio: ivanime.com",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/1 - Temporadas Anteriores',
